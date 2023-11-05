9880657074eb782ef6e6fb94e848ee9710ad5aaa
Mikael Sundell <mikael.sundell@gmail.com>
2023-11-05 14:42:03 +0100

    Project commit

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright 2022-present Contributors to the helloWorld project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/mikaelsundell/helloWorld
+
+#include <iostream>
+
+int
+main(int argc, char **argv)
+{
+    std::cout << "hello, world!" << std::endl;
+}
